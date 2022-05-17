fc830cbafb112bb10cbd20fea8375866e8d98c1a
stanjke <stanjke@gmail.com>
2022-05-17 15:34:53 +0300

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Playstation_logo_colour.svg/2560px-Playstation_logo_colour.svg.png'></img>
+</header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+    <div>
+        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'></img>
+    </div>
+    <div>
+        ava + description
+    </div>
+    <div>
+        My posts
+        <div>
+            New post
+        </div>
+        <div>
+            <div>
+                post 1
+            </div>
+            <div>
+                post 2
+            </div>
+        </div>
+    </div>
+
+</div>
+}
+
+export default Profile;
